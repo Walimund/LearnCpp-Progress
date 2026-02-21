16c834792e1d5da3c5ab9410f02356ded3ced7ef
Walimund <wyatttmonroe@icloud.com>
2026-02-21 01:50:48 -0500

    Initialize Quizzes folder with quiz01Solution

---
+++ Quizzes/Chapter01.cpp
@@ -0,0 +1,22 @@
+#include "Chapter01.h"
+#include <iostream>
+
+
+// Solution for chapter 1 quiz
+void quiz01Solution() {
+    std::cout << "Quiz 1 solution is running...\n";
+
+    std::cout << "Enter an Integer: ";
+
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter another integer: ";
+
+    int y{};
+    std::cin >> y;
+
+    std::cout << x << " + " << y << " is " << x + y << '\n';
+
+    std::cout << x << " - " << y << " is " << x - y << '\n';
+}
--- Quizzes/Chapter01.h
+++ Quizzes/Chapter01.h
@@ -1,1 +1,6 @@
+#ifndef CHAPTER01_H
+#define CHAPTER01_H
 
+void quiz01Solution();
+
+#endif
